14db5167e5cbff3c5c7bc6fefee064674276df49
jeremyfelt <jeremy.felt@gmail.com>
2024-03-27 20:52:59 -0700

    Add built files to repository
    Fixes #40

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => 'e3872954c53053c1b5e7');
