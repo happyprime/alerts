c652cec2fb3535287b743f886fa46c261412792f
jeremyfelt <jeremy.felt@gmail.com>
2024-03-27 21:08:52 -0700

    Update NPM dependencies, apply `npm run build`

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => 'e3872954c53053c1b5e7');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => 'a641ef87932688ef10d5');
