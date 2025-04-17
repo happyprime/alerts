18ed3ba3bbb50335c485e6e1f9d7ef125c247c3a
jeremyfelt <jeremy.felt@gmail.com>
2025-04-17 09:38:43 -0700

    Adjust alert spacing, add built files
    The changes in #31 missed the built CSS/JS. This adds those and
    tweaks a bit more of the spacing.
    
    Fixes #30

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => 'a641ef87932688ef10d5');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => 'c72779110f331b9d5f24');
