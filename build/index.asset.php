eb626f7f796021f8c342798404aef50165101f4c
jeremyfelt <jeremy.felt@gmail.com>
2025-04-17 09:41:04 -0700

    Update wordpress/scripts and rebuild

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => 'c72779110f331b9d5f24');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-hooks', 'wp-html-entities', 'wp-i18n'), 'version' => 'a126f2c50c50169f2918');
